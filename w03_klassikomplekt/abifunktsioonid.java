b0b7f26c318aa0b61970c3dcbbc2c48d0ed1a0ca
Lisanne Järv <lisannejarv@gmail.com>
2021-02-16 12:04:08 +0200

    kolmas tund k'ib

---
+++ w03_klassikomplekt/abifunktsioonid.java
@@ -0,0 +1,9 @@
+package w03_klassikomplekt;
+
+public class abifunktsioonid {
+    public int kolmestSuurem(int a, int b, int c){
+        int s1=a;
+        if(b>s1){s1=b;}
+        return (c>s1)?c:s1;
+    }
+}
