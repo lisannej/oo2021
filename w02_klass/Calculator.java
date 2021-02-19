33be25a8c5911b0ae6ce1f4b540faf186d98da2e
Lisanne Järv <lisannejarv@gmail.com>
2021-02-19 21:57:34 +0200

    kodutoo 2 ei toota

--- w02_klass/Calculator.java
+++ w02_klass/Calculator.java
@@ -8,13 +8,13 @@ public class Calculator {
       double ans;
       char op;
       Scanner reader = new Scanner(System.in);
-      System.out.println("Enter a number: " + num1(6));
-      System.out.println("Enter a number: " + num2(2));
+      System.out.println("Enter a number: " + number.num1(6));
+      System.out.println("Enter a number: " + number.num2(2));
 
-      System.out.print("\nEnter an operator (+, -, *, /): ");
+      System.out.println("\nEnter an operator (+, -, *, /): ");
       
-      System.out.print("\nThe result is given as follows:\n");
-      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+      System.out.println("\nThe result is given as follows:\n");
+      System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
    }
 }
     
