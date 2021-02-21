c062ce4375798ec7e282d46bff83183ad1a9269a
Lisanne Järv <lisannejarv@gmail.com>
2021-02-21 19:43:21 +0200

    kodutoo 2 / pooleli

--- w02_klass/Calculation.java
+++ w02_klass/Calculation.java
@@ -1,26 +1,31 @@
 package w02_klass;
 
-import java.io.Reader;
+import java.util.Scanner;
+   public class Calculation {
+         double num1;
+         double num2;
+         double ans;
+         char op;
 
-public class Calculation {
-    public static void main(String[] args) {
-        double num1;
-        double num2;
-        double ans;
-        char op;
-        System.out.println("\nEnter an operator (+, -, *, /): ");
-        op = reader.next().charAt(0);
-      switch(op) {
-         case '+': ans = num1 + num2;
-            break;
-         case '-': ans = num1 - num2;
-            break;
-         case '*': ans = num1 * num2;
-            break;
-         case '/': ans = num1 / num2;
-            break;
-      default: System.out.printf("Error! Enter correct operator");
-         return;
-        }
-    }
-}
+      op = reader.nextLine().charAt(0);
+      if (op == '+') {
+            ans = (num1 + num2);
+            System.out.println("Answer:" + ans);
+            } 
+      if (op == '-') {
+            ans = (num1 - num2);
+            System.out.println("Answer:" + ans);
+            } 
+      if (op == '*') {
+            ans = (num1 * num2);
+            System.out.println("Answer:" + ans);
+            } 
+      if (op == '/') {
+            ans = (num1/num2);
+            System.out.println("Answer:" + ans);
+            } 
+      else {
+            System.out.println("not implemented yet. Sorry!");
+      }
+   }
+   
--- w02_klass/Calculator.java
+++ w02_klass/Calculator.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 
 public class Calculator {
    public static void main(String[] args) {
-      double num1;
-      double num2;
-      double ans;
-      char op;
       Scanner reader = new Scanner(System.in);
-      System.out.println("Enter a number: " + number.num1(6));
-      System.out.println("Enter a number: " + number.num2(2));
+
+      System.out.println("Enter a number: " );
+      Double num1 = reader.nextDouble();// scanner loeb j'rgmist esinevat double tuupi v''rtust'
+
+      System.out.println("Enter a number: " );
+      Double num2 = reader.nextDouble();
+      reader.nextLine(); // j'rgmine scanner ei tootaks muidu Double ja String tuupi vahe p'rast
 
       System.out.println("\nEnter an operator (+, -, *, /): ");
-      
+      String op = reader.nextLine();
+
+      // close the scanner
+      System.out.println(" ");
+      reader.close();
+      System.out.println(" ");
+
       System.out.println("\nThe result is given as follows:\n");
-      System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
+      System.out.println(num1 + " " + op + " " + num2 + " =" + ans);
    }
 }
     
