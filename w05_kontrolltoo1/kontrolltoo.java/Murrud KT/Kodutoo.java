5421054ba2b8176aa543d900e81f41bacd99c218
Lisanne Järv <lisannejarv@gmail.com>
2021-03-16 11:37:18 +0200

    KT tehtud

---
+++ w05_kontrolltoo1/kontrolltoo.java/Murrud KT/Kodutoo.java
@@ -0,0 +1,11 @@
+
+public class Kodutoo {
+    public static void main(String[] args){
+        Murd esimene = new Murd(12, 5);
+
+        esimene.Segaarv();
+        esimene.Liitmine(1, 2, 2, 3);
+        esimene.Liitmine(1, 3, 1, 3);
+        esimene.Liitmine(4, 5, 2, 3);
+    }
+}
