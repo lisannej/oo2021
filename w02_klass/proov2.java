125f88ff3f2deba0060bf79fade79aa8dc0d62b0
Lisanne Järv <lisannejarv@gmail.com>
2021-05-19 22:55:21 +0300

    klassid

--- w02_klass/proov2.java
+++ w02_klass/proov2.java
@@ -6,7 +6,7 @@ public class proov2 {
         Hashtable<String, Double> ht=new Hashtable<String, Double>();
         ht.put ("Juku", 4.0);
         ht.put ("Kati", 5.0);
-        System.out.println(ht.get("Juku");
+        System.out.println(ht.get("Juku"));
         for(String eesnimi: ht.keySet()){
 			System.out.println(eesnimi + " - "+ht.get(eesnimi));
     }
