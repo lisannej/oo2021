c3c0e78c742cbbd7418874482ccfa56a23985696
Lisanne Järv <lisannejarv@gmail.com>
2021-05-25 21:48:06 +0300

    style

--- w16_eksam/home.php
+++ w16_eksam/home.php
@@ -7,7 +7,7 @@
 	<head>
 		<meta charset = "UTF-8" name = "viewport" content = "width-device=width, initial-scale=1"/>
 		<title>Monitory</title>
-		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+		<link rel = "stylesheet" type = "text/css" href = "style.css" />
 	</head>
 <body>
 
