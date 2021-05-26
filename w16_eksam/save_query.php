0e9ca769040e0a227908b4c4df9318e3f9ea07fb
Lisanne Järv <lisannejarv@gmail.com>
2021-05-26 15:22:06 +0300

    Update save_query.php

--- w16_eksam/save_query.php
+++ w16_eksam/save_query.php
@@ -7,12 +7,11 @@
 	$lastname = $_POST['lastname'];
 	if(ISSET($_POST['signup'])){
 		$check="SELECT username FROM user WHERE username = '$_POST[username]'";
-		if ($check = true)
+		if ($check == true)
 		{
 			echo '<script>alert("User already exists!")</script>';
 			echo '<script>window.location= "index.html"</script>';
-
-}
+		}
 		else{
 			
 			$conn->save($username, $password, $firstname, $lastname);
