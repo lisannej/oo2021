5d842e99f4117fd2c730c3d6f7b40785f4595d52
Lisanne Järv <lisannejarv@gmail.com>
2021-05-26 14:50:33 +0300

    Update save_query.php

--- w16_eksam/save_query.php
+++ w16_eksam/save_query.php
@@ -1,24 +1,25 @@
 <?php
 	require_once "class.php";
 	$conn = new db_class();
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
 	if(ISSET($_POST['signup'])){
 		$sql="select * from user where (username='$username');";
 
-		$res=mysqli_query($mysqli,$sql);
+		//$res=mysqli_query($mysqli,$sql);
 
-		if (mysqli_num_rows($res) > 0) {
+		//if (mysqli_num_rows($res) > 0) {
 			
 			$row = mysqli_fetch_assoc($res);
 			if($username==isset($row['username']))
 			{
 					echo "Username already exists";
 			}
-			}
+			//}
 		else{
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			$firstname = $_POST['firstname'];
-			$lastname = $_POST['lastname'];
+			
 			$conn->save($username, $password, $firstname, $lastname);
 			echo '<script>alert("Successfully saved!")</script>';
 			echo '<script>window.location= "login.html"</script>';
