21093b6f464de75fdc4ddb6df063ac9471023e6f
Lisanne Järv <lisannejarv@gmail.com>
2021-05-26 14:48:02 +0300

    up

--- w16_eksam/save_query.php
+++ w16_eksam/save_query.php
@@ -2,12 +2,27 @@
 	require_once "class.php";
 	$conn = new db_class();
 	if(ISSET($_POST['signup'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$firstname = $_POST['firstname'];
-		$lastname = $_POST['lastname'];
-		$conn->save($username, $password, $firstname, $lastname);
-		echo '<script>alert("Successfully saved!")</script>';
-		echo '<script>window.location= "login.html"</script>';
+		$sql="select * from user where (username='$username');";
+
+		$res=mysqli_query($mysqli,$sql);
+
+		if (mysqli_num_rows($res) > 0) {
+			
+			$row = mysqli_fetch_assoc($res);
+			if($username==isset($row['username']))
+			{
+					echo "Username already exists";
+			}
+			}
+		else{
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$firstname = $_POST['firstname'];
+			$lastname = $_POST['lastname'];
+			$conn->save($username, $password, $firstname, $lastname);
+			echo '<script>alert("Successfully saved!")</script>';
+			echo '<script>window.location= "login.html"</script>';
 	}	
+}
+		
 ?>
