bff3b2c533f214d9904d8e3c0dd384f0bc22b945
Lisanne Järv <lisannejarv@gmail.com>
2021-05-26 15:52:54 +0300

    Update save_query.php

--- w16_eksam/save_query.php
+++ w16_eksam/save_query.php
@@ -1,21 +1,22 @@
 <?php
 	require_once "class.php";
-	$conn = new db_class();
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
 	if(ISSET($_POST['signup'])){
-	$check="SELECT username FROM user WHERE username = '$_POST[username]'";
-		if($check!=false){
-			echo '<script>alert("User already exists!")</script>';
-			echo '<script>window.location= "index.html"</script>';
-		}
-		else{	
-			$conn->save($username, $password, $firstname, $lastname);
-			echo '<script>alert("Successfully saved!")</script>';
-			echo '<script>window.location= "login.html"</script>';
-	}	
+		$conn = new db_class();
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$check = $conn->prepare("SELECT username FROM user WHERE username = '$_POST[username]'");
+		echo $conn->error;
+			if($check!=false){
+				echo '<script>alert("User already exists!")</script>';
+				echo '<script>window.location= "index.html"</script>';
+			}
+			else{	
+				$conn->save($username, $password, $firstname, $lastname);
+				echo '<script>alert("Successfully saved!")</script>';
+				echo '<script>window.location= "login.html"</script>';
+		}	
 }
 		
 ?>
